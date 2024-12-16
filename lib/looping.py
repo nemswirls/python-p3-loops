63ca283a08ca8e8fa04d149ebb610f2adbb8ae3b
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-12-15 20:11:49 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count=10
+    while count>0:
+     print(count)
+     count -=1
+     print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x**2 for x in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   for number in range(1, 101):  # Loop through numbers 1 to 100
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
